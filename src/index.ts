b0f552c97fddb16e8b51355b370c251a00997552
coenicorn <coenicorn.blauwzee@gmail.com>
2024-05-22 11:15:12 +0200

    refactor and comment

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,29 @@
-import { Client, Events, ChatInputCommandInteraction, Collection, GatewayIntentBits, VoiceChannel, REST, Routes, SlashCommandBuilder, CommandInteraction, GuildMember } from "discord.js"
+import {
+    Client,
+    Events,
+    ChatInputCommandInteraction,
+    Collection,
+    GatewayIntentBits,
+    VoiceChannel,
+    REST,
+    Routes,
+    SlashCommandBuilder,
+    CommandInteraction,
+    GuildMember,
+} from "discord.js";
 import {
     joinVoiceChannel,
     createAudioPlayer,
     createAudioResource,
     entersState,
-	StreamType,
-	AudioPlayerStatus,
-	VoiceConnectionStatus,
-	AudioPlayer,
-	VoiceConnection
-} from "@discordjs/voice"
+    StreamType,
+    AudioPlayerStatus,
+    VoiceConnectionStatus,
+    AudioPlayer,
+    VoiceConnection,
+} from "@discordjs/voice";
 import { createDiscordJSAdapter } from "./adapter.js";
-import * as dotenv from "dotenv"
+import * as dotenv from "dotenv";
 
 dotenv.config();
 
@@ -25,249 +37,324 @@ const rest = new REST().setToken(token);
 const clientId = process.env.CLIENTID!;
 
 interface Command {
-	execute(interaction: CommandInteraction): Promise<void>;
-	data: any;
+    execute(interaction: CommandInteraction): Promise<void>;
+    data: any;
 }
 
 const commands: Collection<string, Command> = new Collection();
 
-function registerCommand(command: Command)
-{
-	const name = command.data.name;
-	commands.set(name, command);
+function registerCommand(command: Command) {
+    const name = command.data.name;
+    commands.set(name, command);
 }
 
-async function connectToChannel(channel: VoiceChannel): Promise<VoiceConnection> {
-	const connection = joinVoiceChannel({
-		channelId: channel.id,
-		guildId: channel.guild.id,
-		adapterCreator: createDiscordJSAdapter(channel),
-	});
+async function connectToChannel(
+    channel: VoiceChannel
+): Promise<VoiceConnection> {
+    const connection = joinVoiceChannel({
+        channelId: channel.id,
+        guildId: channel.guild.id,
+        adapterCreator: createDiscordJSAdapter(channel),
+    });
 
-	try {
-		await entersState(connection, VoiceConnectionStatus.Ready, 30_000);
-		return connection;
-	} catch (error) {
-		connection.destroy();
-		throw error;
-	}
+    try {
+        await entersState(connection, VoiceConnectionStatus.Ready, 30_000);
+        return connection;
+    } catch (error) {
+        connection.destroy();
+        throw error;
+    }
 }
 
 interface AudioGuildData {
-	player: AudioPlayer;
-	connection: VoiceConnection;
-	nowPlaying: string;
+    player: AudioPlayer;
+    connection: VoiceConnection;
+    nowPlaying: string /* name of the currently playing stream */;
+    channelId: string /* channel ID of the channel currently playing in */;
 }
 
 const guildPlayers: Map<string, AudioGuildData> = new Collection();
 
 interface RadioURL {
-	name: string;
-	value: string;
+    name: string;
+    value: string;
 }
 
 const radioURLS: Array<RadioURL> = [
-	{ name: 'indie', value: 'http://streams.pinguinradio.com/PinguinRadio192.mp3'},
-	{ name: 'classics', value: 'http://streams.pinguinradio.com/PinguinClassics192.mp3'},
-	{ name: 'on the rocks', value: 'http://streams.pinguinradio.com/PinguinOnTheRocks192.mp3'},
-	{ name: 'aardschok', value: 'https://streams.pinguinradio.com/Aardschok192.mp3'},
-	{ name: 'pop', value: 'https://samcloud.spacial.com/api/listen?sid=98586&m=sc&rid=174409'},
-	{ name: 'grooves', value: 'https://samcloud.spacial.com/api/listen?sid=98587&m=sc&rid=174412'},
-	{ name: 'pluche', value: 'https://samcloud.spacial.com/api/listen?sid=98569&m=sc&rid=174384'},
-	{ name: 'world', value: 'https://samcloud.spacial.com/api/listen?sid=98570&m=sc&rid=174387'},
-	{ name: 'fiesta', value: 'https://19293.live.streamtheworld.com/SP_R2292843_SC'},
-	{ name: 'showcases', value: 'https://samcloud.spacial.com/api/listen?sid=110690&m=sc&rid=190799&t=ssl'},
-	{ name: 'vintage', value: 'https://samcloud.spacial.com/api/listen?sid=131111&m=sc&rid=275910&t=ssl'},
-	{ name: 'blues', value: 'https://samcloud.spacial.com/api/listen?sid=93462&m=sc&rid=168006&t=ssl'},
+    {
+        name: "indie",
+        value: "http://streams.pinguinradio.com/PinguinRadio192.mp3",
+    },
+    {
+        name: "classics",
+        value: "http://streams.pinguinradio.com/PinguinClassics192.mp3",
+    },
+    {
+        name: "on the rocks",
+        value: "http://streams.pinguinradio.com/PinguinOnTheRocks192.mp3",
+    },
+    {
+        name: "aardschok",
+        value: "https://streams.pinguinradio.com/Aardschok192.mp3",
+    },
+    {
+        name: "pop",
+        value: "https://samcloud.spacial.com/api/listen?sid=98586&m=sc&rid=174409",
+    },
+    {
+        name: "grooves",
+        value: "https://samcloud.spacial.com/api/listen?sid=98587&m=sc&rid=174412",
+    },
+    {
+        name: "pluche",
+        value: "https://samcloud.spacial.com/api/listen?sid=98569&m=sc&rid=174384",
+    },
+    {
+        name: "world",
+        value: "https://samcloud.spacial.com/api/listen?sid=98570&m=sc&rid=174387",
+    },
+    {
+        name: "fiesta",
+        value: "https://19293.live.streamtheworld.com/SP_R2292843_SC",
+    },
+    {
+        name: "showcases",
+        value: "https://samcloud.spacial.com/api/listen?sid=110690&m=sc&rid=190799&t=ssl",
+    },
+    {
+        name: "vintage",
+        value: "https://samcloud.spacial.com/api/listen?sid=131111&m=sc&rid=275910&t=ssl",
+    },
+    {
+        name: "blues",
+        value: "https://samcloud.spacial.com/api/listen?sid=93462&m=sc&rid=168006&t=ssl",
+    },
 ];
 
 registerCommand({
-	data: new SlashCommandBuilder()
-		.setName('radio')
-		.setDescription('begin playing a radio station')
-		.addIntegerOption(option => 
-			option.addChoices(
-				{ name: 'indie', value: 0},
-				{ name: 'classics', value: 1},
-				{ name: 'on the rocks', value: 2},
-				{ name: 'aardschok', value: 3},
-				{ name: 'pop', value: 4},
-				{ name: 'grooves', value: 5},
-				{ name: 'pluche', value: 6},
-				{ name: 'world', value: 7},
-				{ name: 'fiesta', value: 8},
-				{ name: 'showcases', value: 9},
-				{ name: 'vintage', value: 10},
-				{ name: 'blues', value: 11},
-			)
-			.setName('station')
-			.setRequired(true)
-			.setDescription('the station that will play')
-		),
-	async execute(interaction: ChatInputCommandInteraction) {
-	
-		try {
-			const guildid = interaction.guildId!;
-			const member = interaction.member as GuildMember | null;
-			const user = interaction.user;
+    data: new SlashCommandBuilder()
+        .setName("radio")
+        .setDescription("begin playing a radio station")
+        .addIntegerOption((option) =>
+            option
+                .addChoices(
+                    { name: "indie", value: 0 },
+                    { name: "classics", value: 1 },
+                    { name: "on the rocks", value: 2 },
+                    { name: "aardschok", value: 3 },
+                    { name: "pop", value: 4 },
+                    { name: "grooves", value: 5 },
+                    { name: "pluche", value: 6 },
+                    { name: "world", value: 7 },
+                    { name: "fiesta", value: 8 },
+                    { name: "showcases", value: 9 },
+                    { name: "vintage", value: 10 },
+                    { name: "blues", value: 11 }
+                )
+                .setName("station")
+                .setRequired(true)
+                .setDescription("the station that will play")
+        ),
+    async execute(interaction: ChatInputCommandInteraction) {
+        try {
+            // grab important data
+            const guildid = interaction.guildId!;
+            const member = interaction.member as GuildMember | null;
+            const user = interaction.user;
 
-			// dunno how this would fail, check for it nonetheless
-			if (!member) {
-				interaction.reply("failed to fetch interaction member");
-				return;
-			}
+            // dunno how this would fail, check for it nonetheless
+            if (!member) {
+                interaction.reply("failed to fetch interaction member");
 
-			// I hate typescript
-			const channel = member.voice.channel as VoiceChannel | null;
+                return;
+            }
 
-			if (!channel) {
-				await interaction.reply("you are not in any voice channels");
+            // I hate typescript
+            const channel = member.voice.channel as VoiceChannel | null;
 
-				return;
-			}
+            if (!channel) {
+                await interaction.reply("you are not in any voice channels");
 
-			const url = radioURLS[interaction.options.getInteger('station', true)];
+                return;
+            }
 
-			let data = guildPlayers.get(guildid);
+            const url =
+                radioURLS[interaction.options.getInteger("station", true)];
 
-			if (data?.nowPlaying === url.name)
-			{
-				interaction.reply(user.toString() + ` Already playing ___${url.name}___`);
+            let data = guildPlayers.get(guildid);
 
-				return;
-			} else {
-				// new channel has been picked
-				interaction.reply(user.toString() + ` Playing ___${url.name}___`);
-			}
+            // if nothing's currently playing, create new data object
+            if (data === undefined) {
+                data = {
+                    player: createAudioPlayer(),
+                    connection: await connectToChannel(channel),
+                    nowPlaying: "",
+                    channelId: channel.id,
+                };
+            }
 
-			if (!data) {
-				data = {
-					player: createAudioPlayer(),
-					connection: await connectToChannel(channel),
-					nowPlaying: url.name
-				};
-			} else {
-				data.nowPlaying = url.name;
-			}
+            if (data.channelId !== channel.id) {
+                // user is in another channel, move
+                await data.connection.destroy();
+                data.connection = await connectToChannel(channel);
+            }
 
-			const resource = createAudioResource(url.value, {
-				inputType: StreamType.Arbitrary
-			});
+            if (data.nowPlaying === url.name) {
+                // song is currenly being played in the same channel, do nothing
+                interaction.reply(
+                    user.toString() + ` Already playing ${data.nowPlaying}!`
+                );
 
-			data.player.play(resource);
+                return;
+            }
 
-			entersState(data.player, AudioPlayerStatus.Playing, 5000);
+            // black magic 0_0
+            const resource = createAudioResource(url.value, {
+                inputType: StreamType.Arbitrary,
+            });
 
-			data.connection.subscribe(data.player);
+            data.player.play(resource);
 
-			guildPlayers.set(guildid, data);
+            entersState(data.player, AudioPlayerStatus.Playing, 5000);
 
-		} catch (e) {
-			console.error(e);
-		}
-	}
+            data.connection.subscribe(data.player);
+            data.nowPlaying = url.name;
+
+            // update stored data
+            guildPlayers.set(guildid, data);
+
+            interaction.reply(user.toString() + ` Playing ___${url.name}___`);
+        } catch (e) {
+            console.error(e);
+        }
+    },
 });
 
 registerCommand({
-	data: new SlashCommandBuilder()
-		.setName('stop')
-		.setDescription('disconnects from voice channel'),
-	async execute(interaction: ChatInputCommandInteraction) {
-		const guildId = interaction.guildId!;
-		const user = interaction.user;
+    data: new SlashCommandBuilder()
+        .setName("stop")
+        .setDescription("disconnects from voice channel"),
+    async execute(interaction: ChatInputCommandInteraction) {
+        const guildId = interaction.guildId!;
+        const user = interaction.user;
 
-		const guildPlayer = guildPlayers.get(guildId);
+        const data = guildPlayers.get(guildId);
 
-		if (guildPlayer === undefined) {
-			interaction.reply({content: user.toString() + " Not in a voice channel", ephemeral: true});
-		} else {
-			guildPlayer.connection.destroy();
-			guildPlayer.player.stop();
+        if (data === undefined) {
+            // data for guild does not exist
+            interaction.reply({
+                content: user.toString() + " Not in a voice channel",
+                ephemeral: true,
+            });
+        } else {
+            // stop playing
+            data.connection.destroy();
+            data.player.stop();
 
-			interaction.reply(user.toString() + " Done!");
-		}
-	}
+            interaction.reply(user.toString() + " Done!");
+        }
+    },
 });
 
+/* registers a new slash command */
 registerCommand({
-	data: new SlashCommandBuilder()
-		.setName('nowplaying')
-		.setDescription('list currently playing radio channel'),
-	async execute(interaction: ChatInputCommandInteraction) {
-		const guildid = interaction.guildId;
-		const user = interaction.user;
+    data: new SlashCommandBuilder()
+        .setName("nowplaying")
+        .setDescription("list currently playing radio channel"),
+    async execute(interaction: ChatInputCommandInteraction) {
+        const guildid = interaction.guildId;
+        const user = interaction.user;
 
-		if (!guildid) {
-			interaction.reply('Not playing anything :/');
-			return;
-		}
-		
-		const data = guildPlayers.get(guildid!);
+        if (!guildid) {
+            interaction.reply("Not playing anything :/");
+            return;
+        }
 
-		if (!data) {
-			interaction.reply(user.toString() + ' Not playing anything :/');
-			return;
-		}
+        const data = guildPlayers.get(guildid!);
 
-		interaction.reply(user.toString() + ` Currently playing ___${data?.nowPlaying}___`);
+        if (!data) {
+            interaction.reply(user.toString() + " Not playing anything :/");
+            return;
+        }
 
-		
-	}
+        interaction.reply(
+            user.toString() + ` Currently playing ___${data.nowPlaying}___`
+        );
+    },
 });
 
-// and deploy your commands!
+// deploy slash commands (blatantly ripped from discord's tutorial lmao)
 (async () => {
-	const commandList: Array<string> = [];
+    const commandList: Array<string> = [];
 
-	commands.each(c => {
-		commandList.push(c.data.toJSON());
-		console.log(`deployed command '${c.data.name}'`);
-	})
+    commands.each((c) => {
+        commandList.push(c.data.toJSON());
+        console.log(`deployed command '${c.data.name}'`);
+    });
 
-	const rest = new REST().setToken(token);
+    const rest = new REST().setToken(token);
 
-	try {
-		// The put method is used to fully refresh all commands in the guild with the current set
-		const data = await rest.put(
-			Routes.applicationCommands(clientId),
-			{ body: commandList },
-		);
-	} catch (error) {
-		// And of course, make sure you catch and log any errors!
-		console.error(error);
-	}
+    try {
+        // The put method is used to fully refresh all commands in the guild with the current set
+        const data = await rest.put(Routes.applicationCommands(clientId), {
+            body: commandList,
+        });
+    } catch (error) {
+        console.error(error);
+    }
 })();
 
-const client = new Client({ intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildVoiceStates,
-	GatewayIntentBits.GuildMessages
-] });
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildVoiceStates,
+        GatewayIntentBits.GuildMessages,
+    ],
+});
 
 client.login(token);
 
-client.on('ready', async () => {
-	console.log("client ready");
+client.on("ready", async () => {
+    console.log(
+        `\n****************\nClient ready\nTOKEN=${token}\nCLIENTID=${clientId}\n****************\n`
+    );
 });
 
+// also blatantly stolen from discord (but it's unlicensed so who cares >_>) (I really really REALLY hope it's actually unlicensed)
 client.on(Events.InteractionCreate, async (interaction) => {
-	if (!interaction.isChatInputCommand()) return;
+    if (!interaction.isChatInputCommand()) return;
 
-	const command = commands.find((elm) => elm.data.name == interaction.commandName);
+    const command = commands.find(
+        (elm) => elm.data.name == interaction.commandName
+    );
 
-	if (!command) {
-		interaction.reply("you are NOT going to like this... but 404");
-		return;
-	}
+    if (!command) {
+        console.log(
+            `command ${interaction.commandName} not found (used by @${interaction.user.displayName})`
+        );
+        interaction.reply({
+            content:
+                interaction.user.toString() +
+                ` A teeny weeny error occurred, message @coenicorn he'll probably want to know`,
+            ephemeral: true,
+        });
+        return;
+    }
 
-	try {
-		await command.execute(interaction);
-	} catch (e) {
-		console.error(e);
-		if (interaction.replied || interaction.deferred) {
-			await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
-		} else {
-			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-		}
-	}
+    try {
+        await command.execute(interaction);
+    } catch (e) {
+        console.error(e);
+        if (interaction.replied || interaction.deferred) {
+            await interaction.followUp({
+                content: "There was an error while executing this command!",
+                ephemeral: true,
+            });
+        } else {
+            await interaction.reply({
+                content: "There was an error while executing this command!",
+                ephemeral: true,
+            });
+        }
+    }
 });
